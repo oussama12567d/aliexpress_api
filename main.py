c7229ac26f984daa1db4280acbd2c87d27a34867
Djedidi oussama <139490003+oussama12567d@users.noreply.github.com>
2023-09-17 16:27:29 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,11 +1,27 @@
+import time
 from fastapi import FastAPI, HTTPException
 from pydantic import BaseModel
 from aliexpress_api import AliexpressApi, models
+import requests,re
+
+
 
 API_KEY  = '33792213'
 API_SECRET  = '23d6195dce2267ff0fa0b7ef755ecf36'
 aliexpress = AliexpressApi(API_KEY, API_SECRET, models.Language.EN, models.Currency.EUR, "default")
 app = FastAPI()
+
+
+
+
+
+
+ 
+
+   
+    
+
+
 
 def addsourceType(input_string):
     # Use the split() method to split the string at the "?" mark
@@ -13,13 +29,14 @@ def addsourceType(input_string):
 
 def make_affiliate_links(input_string):
     print('make_affiliate_links called')
-    affiliate_links = aliexpress.get_affiliate_links(input_string)
+    affiliate_links = aliexpress.get_affiliate_links((input_string))
+    
     return affiliate_links[0].promotion_link
 
 
 def get_product_details(input_string):
     print('get_product_details called')
-    products = aliexpress.get_products_details([API_KEY, input_string])
+    products = aliexpress.get_products_details(input_string)
     return products[0]
 
 class Requiest (BaseModel):
@@ -42,13 +59,13 @@ async def start():
 # Create an endpoint to create an item
 @app.post("/get-affiliat-link/", response_model=Response)
 async def get_link(item: Requiest):
-    aff_link =  addsourceType(make_affiliate_links((item.link)))
+    aff_link = addsourceType(make_affiliate_links((item.link)))
     shop_link = make_affiliate_links("https://www.aliexpress.com/p/shoppingcart/index.html")
-    product = get_product_details(item.link)
+    product = get_product_details((item.link))
     return  Response(aff=aff_link , shop=shop_link , name=product.product_title,img=product.product_main_image_url)
 
 
 
-if __name__ == "__main__":
+if __name__ == "__main__":    
     import uvicorn
     uvicorn.run(app, host="0.0.0.0", port=8000)
